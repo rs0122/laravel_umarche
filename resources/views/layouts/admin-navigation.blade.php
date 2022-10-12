51a3442176d479fa01b44b6670156efd7243ee8f
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-10-12 20:02:35 +0900

    shopCreateDelete

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -18,10 +18,10 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
-                        オーナー管理
+                        オーナー一覧
                     </x-nav-link>
                     <x-nav-link :href="route('admin.expired-owners.index')" :active="request()->routeIs('admin.expired-owners.index')">
-                        オーナー管理
+                        期限切れオーナー一覧
                     </x-nav-link>
                 </div>
             </div>
