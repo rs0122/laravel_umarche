2c24ec8aa981c2f3568e4ba8f3e00b2d9efd8d4f
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-10-24 19:06:49 +0900

    image delete

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
+use Illuminate\Support\Facades\Storage;
 use App\Services\ImageService;
 
 class ImageController extends Controller
@@ -94,14 +95,20 @@ class ImageController extends Controller
         ->with(['message' => '画像情報を更新しました。', 'status' => 'info']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/' . $image->filename;
+
+        if(Storage::exists($filePath)){
+            Storage::delete($filePath);
+        }
+
+        Image::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' =>'画像を削除しました。',
+            'status' => 'alert']);
     }
 }
