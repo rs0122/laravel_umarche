87578a0201d2e1762a7bf77e17d1ba341f01e138
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-11-06 14:48:13 +0900

    route, itemcontroller, index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
+            UserSeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class,
         ]);
