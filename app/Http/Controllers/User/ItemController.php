b59e1f3eae79562cd3b2abb13d04aac48f53c769
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-11-10 18:54:06 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use App\Models\Product;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index(){
         $stocks = DB::table('t_stocks')
             ->select('product_id',
@@ -37,5 +42,11 @@ class ItemController extends Controller
         // $products = Product::all();
 
         return view('user.index', compact('products'));
+    }
+
+    public function show($id){
+        $product = Product::findOrFail($id);
+
+        return view('user.show', compact('product'));
     }
 }
