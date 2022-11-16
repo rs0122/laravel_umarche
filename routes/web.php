40d4b7aaa6f5d33c085a662ca556159d5c398f65
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-11-16 21:08:29 +0900

    cart view delete

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,11 @@ Route::middleware('auth:users')->group(function(){
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-Route::prefix('shops')->
+Route::prefix('cart')->
     middleware('auth:users')->group(function(){
+        Route::get('/', [CartController::class, 'index'])->name('cart.index');
         Route::post('add', [CartController::class, 'add'])->name('cart.add');
+        Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
     }
 );
 
